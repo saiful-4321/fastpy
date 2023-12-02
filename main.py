80cc886568277f81e9ffa49133a9bcecce8fa115
Saiful-5005303 <bd.saif321@gmail.com>
2023-12-02 21:37:09 +0600

    created about route

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ app = FastAPI()
 @app.get('/')
 def index():
     return { 
+        'page': 'index',
         'data': {
             'name': 'Saiful', 
             'designation': 'SDE'
         },
         'status': 200
+    }
+
+@app.get('/about')
+def about():
+    return {
+        'page': 'about',
+        'status': 200,
+        'data': {
+            'name': 'saiful islam',
+            'from': 'lakshmipur'
+        }
     }

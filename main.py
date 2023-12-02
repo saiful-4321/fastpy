dd64326ae3ec34dbea51ff3f9e5602f06d1c46c7
Saiful-5005303 <bd.saif321@gmail.com>
2023-12-02 21:58:10 +0600

    dynamic url management

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-
 def index():
     return { 
         'page': 'index',
+        'title': 'blog list',
         'data': {
             'name': 'Saiful', 
             'designation': 'SDE'
         },
         'status': 200
+    }
+
+@app.get('/blogs/{id}')
+def show_blog(id):
+    return {
+        'page': 'single blog',
+        'id': id,
+        'data': {
+            'title': 'blog 1',
+            'details': 'This is the details for blog no: ' + id
+        }
     }
 
 @app.get('/about')

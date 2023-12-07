7567ad0a073719af706fa8230169636d5e17b827
Saiful-5005303 <bd.saif321@gmail.com>
2023-12-07 23:23:13 +0600

    cleared up the repo

--- test.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-inst = FastAPI()
-
-#to start the server
-#here command will be uvicorn filename:fastApiInstanceName --realod
-@inst.get('/')
-def index():
-    return "index here"

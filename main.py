69b4bb053cff67d7ebf968315b1b5bbb88be8e5b
Saiful-5005303 <bd.saif321@gmail.com>
2023-12-22 23:13:16 +0600

    way to handle body params

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
+
+@app.post("/createposts")
+def createposts(payload: dict = Body(...)):
+    return payload

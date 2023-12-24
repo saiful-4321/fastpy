d55bbb3f2eca0d269b5c9cb13df6246f6843f046
Saiful-5005303 <bd.saif321@gmail.com>
2023-12-24 21:53:09 +0600

    handled not found exception

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -34,7 +34,7 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 async def createposts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 10000000)
@@ -52,6 +52,21 @@ async def get_latest_post():
     return {"Post": post}
 
 @app.get("/posts/{id}")
-async def get_post(id: int):
+async def get_post(id: int, response: Response):
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with Id: {id} was not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"post with Id: {id} was not found"}
     return {"post": post}
+
+def find_index_post(id: int):
+    for index, post in enumerate(all_posts):
+        if post['id'] == id:
+            return index
+
+@app.delete("/posts/{id}")
+async def delete_post(id: int):
+    index = find_index_post(id=id)
+    all_posts.pop(index)
+    return {'message': 'Post successfully removed'}
